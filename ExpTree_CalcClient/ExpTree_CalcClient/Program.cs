481f4b68475e5d3501544af1b03c108caafc5c9a
Zevn9 <dz3nls7@gmail.com>
2021-02-16 08:23:49 +0300

    create exp tree client

---
+++ ExpTree_CalcClient/ExpTree_CalcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpTree_CalcClient
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите выражение");
+      string expression = Console.ReadLine();
+      //убираем пробелы и преобразуем строку в массив символов
+      var mas = expression.Replace(" ", "").ToCharArray();
+
+      Expression Tree = ExpressionTree.ParsingExpression(mas);
+      Console.WriteLine("Получившееся дерево");
+      Console.WriteLine(Tree.ToString());
+      //считаем
+      var ans = Calculating.Calculate(Tree).Result;
+      Console.WriteLine("Ответ: " + ans.ToString());
+    }
+  }
+}
